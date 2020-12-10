fb5a6a0b778761c8cb2d944dfd028a6c06f50ee3
Dmytro Orlov <orlov.dm@gmail.com>
2020-12-10 23:25:44 +0100

    2020-12-10-23-25-44

--- src/main/scala/elevator/ElevatorControlSystem.scala
+++ src/main/scala/elevator/ElevatorControlSystem.scala
@@ -38,50 +38,48 @@ object ElevatorControlSystem {
       initial <- ecsState.get
       _ <- IO.fail(ElevatorErr.maxElevatorExceeded(initial.elevators.size))
         .when(initial.elevators.size > 16)
-      res <- IO.succeed {
-        new ElevatorControlSystem {
-          def step() = ecsState.update { s =>
+      res = new ElevatorControlSystem {
+        def step() = ecsState.update { s =>
 
-            s
+
+          s
+        }
+
+        def dropOff(id: ElevatorId, floor: Floor) =
+          ecsState.updateSome {
+            case s if s.elevators.contains(id) =>
+              val e = s.elevators(id)
+              s.copy(elevators = s.elevators + (e.id -> e.copy(dropOffs = e.dropOffs + floor)))
           }
 
-          def dropOff(id: ElevatorId, floor: Floor) =
-            ecsState.updateSome {
-              case s if s.elevators.contains(id) =>
-                val e = s.elevators(id)
-                s.copy(elevators = s.elevators + (e.id -> e.copy(dropOffs = e.dropOffs + floor)))
-            }
+        def pickUp(floor: Floor, direction: Direction) =
+          ecsState.update { s =>
+            val updatedPickUps = s.copy(pickUps = s.pickUps + (floor -> direction))
 
-          def pickUp(floor: Floor, direction: Direction) =
-            ecsState.update { s =>
-              val updatedPickUps = s.copy(pickUps = s.pickUps + (floor -> direction))
+            if (!someoneShouldStop(s, floor, direction)) {
+              collectIdleElevator(s, floor).map { e =>
+                idleStartsMoving(e, floor, direction)
+              }.fold(updatedPickUps) { idleElevator =>
+                updatedPickUps.copy(elevators = updatedPickUps.elevators + (idleElevator.id -> idleElevator))
+              }
+            } else updatedPickUps
+          }
 
-              if (!someoneShouldStop(s, floor, direction)) {
-                collectIdleElevator(s, floor).map { e =>
-                  idleStartsMoving(e, floor, direction)
-                }.fold(updatedPickUps) { idleElevator =>
-                  updatedPickUps.copy(elevators = updatedPickUps.elevators + (idleElevator.id -> idleElevator))
-                }
-              } else updatedPickUps
-            }
+        def status = ecsState.get
 
-          def status = ecsState.get
+        def someoneShouldStop(state: EcsState, floor: Floor, direction: Direction) =
+          state.elevators.values.exists { e =>
+            direction == e.direction &&
+              (direction == Up && e.dropOffs.max >= floor || direction == Down && e.dropOffs.min <= floor)
+          }
 
-          def someoneShouldStop(state: EcsState, floor: Floor, direction: Direction) =
-            state.elevators.values.exists { e =>
-              direction == e.direction &&
-                (direction == Up && e.dropOffs.max >= floor || direction == Down && e.dropOffs.min <= floor)
-            }
+        def collectIdleElevator(state: EcsState, floor: Floor) =
+          state.elevators.values.filter(_.dropOffs.isEmpty).minByOption(_.currFloor - floor)
 
-          def collectIdleElevator(state: EcsState, floor: Floor) =
-            state.elevators.values.filter(_.dropOffs.isEmpty).minByOption(_.currFloor - floor)
-
-          def idleStartsMoving(e: ElevatorState, floor: Floor, direction: Direction) =
-            e.copy(dropOffs = e.dropOffs + floor, direction = if (e.currFloor < floor) Up else Down)
-        }
+        def idleStartsMoving(e: ElevatorState, floor: Floor, direction: Direction) =
+          e.copy(dropOffs = e.dropOffs + floor, direction = if (e.currFloor < floor) Up else Down)
       }
-    }
-      yield res
+    } yield res
 }
 
 case class AppCfg(elevatorMax: Int)
